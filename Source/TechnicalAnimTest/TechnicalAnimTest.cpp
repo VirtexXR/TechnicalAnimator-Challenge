3d33c1fe5bfc569ae0cd96b9b06c3c5e41f1d226
Tim Mcguinness <33485538+Tim-McGuinness@users.noreply.github.com>
2022-08-22 15:14:38 +0100

    Init

---
+++ Source/TechnicalAnimTest/TechnicalAnimTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TechnicalAnimTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TechnicalAnimTest, "TechnicalAnimTest" );
---
+++ Source/TechnicalAnimTest/TechnicalAnimTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TechnicalAnimTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECHNICALANIMTEST_API ATechnicalAnimTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
